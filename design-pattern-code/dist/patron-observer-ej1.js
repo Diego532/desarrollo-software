fee61f266f9849317c1ee506cad61d23dd43ee8a
Diemibas <diegobastardo1@gmail.com>
2022-05-27 19:20:46 -0400

    Creacion de concrete subject y observer

--- design-pattern-code/dist/patron-observer-ej1.js
+++ design-pattern-code/dist/patron-observer-ej1.js
@@ -1,9 +1,9 @@
-var concreteSubject = /** @class */ (function () {
-    function concreteSubject() {
+var ConcreteSubject = /** @class */ (function () {
+    function ConcreteSubject() {
         this.observers = [];
     }
     // Los metodos de manejo de la suscripcion
-    concreteSubject.prototype.addObservador = function (observer) {
+    ConcreteSubject.prototype.addObservador = function (observer) {
         var isExist = this.observers.includes(observer); // investigar metodo de array includes
         if (isExist) {
             return console.log('El suscriptor que se quiere agregar ya existe');
@@ -11,5 +11,36 @@ var concreteSubject = /** @class */ (function () {
         console.log('Subject: Agrego un observador');
         this.observers.push(observer);
     };
-    return concreteSubject;
+    ConcreteSubject.prototype.removeObservador = function (observer) {
+        var observadorIndex = this.observers.indexOf(observer);
+        if (observadorIndex === -1) {
+            return console.log('Observador no existe');
+        }
+        this.observers.slice(observadorIndex, 1);
+        console.log("El observador ha eliminado su suscripcion");
+    };
+    ConcreteSubject.prototype.notify = function () {
+        console.log('Subject notificando observadores...');
+        for (var _i = 0, _a = this.observers; _i < _a.length; _i++) {
+            var observador = _a[_i];
+            observador.update(this); // Error en la definicion d elos parametros en la interface observer y subject
+        }
+    };
+    ConcreteSubject.prototype.someBussinessLogic = function () {
+        console.log('Estoy haciendo algo importante');
+        this.state = Math.floor(Math.random() * (10 + 1));
+        console.log("My state  has just changed to " + this.state);
+        this.notify();
+    };
+    return ConcreteSubject;
+}());
+var ConcreteObserverA = /** @class */ (function () {
+    function ConcreteObserverA() {
+    }
+    ConcreteObserverA.prototype.update = function (subject) {
+        if (subject instanceof ConcreteSubject && subject.state < 3) {
+            console.log('ConcreteObserverA: Reacted to the event.');
+        }
+    };
+    return ConcreteObserverA;
 }());
--- design-pattern-code/patron-observer-ej1.ts
+++ design-pattern-code/patron-observer-ej1.ts
@@ -1,21 +1,21 @@
 interface Subject {
-    addObervador(observer: Observer): void;
+    addObservador(observer: Observer): void;
     removeObservador(observer: Observer): void;
     notify():void;
 }
 
-interface Observer{
-    //Recibe update de subject.
-    update(subject: Subject): void;
+interface Observer {
+    update(subject: Subject):void;
 }
 
-class concreteSubject implements Subject{
+class ConcreteSubject implements Subject{
 
+    
     public state: number;
     private observers: Observer[] = [];
 
     // Los metodos de manejo de la suscripcion
-    addObservador(observer: Observer): void{
+    public addObservador(observer: Observer): void{
         const isExist = this.observers.includes(observer); // investigar metodo de array includes
         if (isExist){
             return console.log('El suscriptor que se quiere agregar ya existe');
@@ -25,5 +25,44 @@ class concreteSubject implements Subject{
         this.observers.push(observer);
     }
 
+    public removeObservador(observer: Observer): void {
+        const observadorIndex = this.observers.indexOf(observer);
+        if (observadorIndex === -1){
+            return console.log('Observador no existe');
+        }
+        this.observers.slice(observadorIndex,1);
+        console.log("El observador ha eliminado su suscripcion");
+        
+    } 
+
+    public notify(): void {
+        console.log('Subject notificando observadores...');
+        for (const observador of this.observers){
+            observador.update(this); // Error en la definicion d elos parametros en la interface observer y subject
+        }
+    }
+
+    public someBussinessLogic() : void{
+        console.log('Estoy haciendo algo importante');
+        this.state = Math.floor(Math.random()*(10 + 1));
+
+        console.log(`My state  has just changed to ${this.state}`);
+        this.notify();
+    }
+
 }
+
+class ConcreteObserverA implements Observer{
+    public update(subject: Subject): void {
+        if (subject instanceof ConcreteSubject && subject.state < 3) {
+            console.log('ConcreteObserverA: Reacted to the event.');
+        }
+    }
+
+}
+
+
+
+
+
 
